e8c3dd66e8384ee05131628fd62e1dabf2d44800
leshchenko1979 <60138889+leshchenko1979@users.noreply.github.com>
2020-08-02 17:50:36 +0300

    new logging and exception capture

--- superscript.py
+++ superscript.py
@@ -1,4 +1,18 @@
-import postponed
-import assign_patch
-import tasks
-import download
+from lxutils.log import timer, log, exception
+
+def main():
+    with timer('Переношу отложенные с наступившим сроком возврата в квалификацию'):
+        import postponed
+    with timer('Назначаю ответственных по сделкам ответственными по контактам'):
+        import assign_patch
+    with timer('Проставляю задачи по сделкам в работе без активных задач'):
+        import tasks
+    with timer('Сохраняю данные для отчетов Tableau'):
+        import download
+
+try:
+    exit(main())
+except Exception:
+    exception("Exception in main()")
+    exit(1)
+
