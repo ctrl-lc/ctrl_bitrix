409ac076f9e46bda52786a04ade2cae255876661
Алексей Лещенко <a.leshchenko@ctrl.lc>
2020-08-15 11:25:18 +0300

    переходим на апдейт руками

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
-pip install --upgrade fast-bitrix24
-pip install --upgrade lxutils
-git stash --all
-git pull
-git stash pop
+rem pip install --upgrade fast-bitrix24
+rem pip install --upgrade lxutils
+rem git stash --all
+rem git pull
+rem git stash pop
 python.exe superscript.py
