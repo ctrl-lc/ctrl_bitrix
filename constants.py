d6f35e19cf21b6c8e42ccfdccbd4b0a49311287c
Alexey Leshchenko <leshchenko@gmail.com>
2021-03-11 10:55:14 +0300

    Имя формы + красота

--- constants.py
+++ constants.py
@@ -1,11 +1,12 @@
 class Stage(object):
-    CALL_CENTER = 'NEW'
+    CALL_CENTER = "NEW"
     POSTPONED = 9
-    LOST = 'LOSE'
+    LOST = "LOSE"
 
 
 class Field(object):
     # последний продавец до возврата из отложенных в КЦ
-    LAST_SALESMAN = 'UF_CRM_1610451561'
-    BRING_BACK_DUE = 'UF_CRM_1582643149318'  # дата возврата из отложенных
-    REASON = 'UF_CRM_1579180371132'  # причина отказа
+    LAST_SALESMAN = "UF_CRM_1610451561"
+    BRING_BACK_DUE = "UF_CRM_1582643149318"  # дата возврата из отложенных
+    REASON = "UF_CRM_1579180371132"  # причина отказа
+    FORM_NAME = "UF_CRM_5E20307D5B33E"  # название формы, с которой пришёл лид
